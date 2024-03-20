9e9632fcd5e294fc732b9f835909914d19d968cd
Queenie <quyenquyen21@hotmail.com>
2024-03-19 23:18:40 -0400

    Add the Individual Product Page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,51 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Home from './home/Home'
-import About from './aboutPage/About'
-import Contact from './contactPage/Contact'
-import Login from './signinPage/signin';
-import CreateAccount from './createaacountPage/createAccount'
-import CartPage from './cartPage/CartPage';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Home from "./home/Home";
+import About from "./aboutPage/About";
+import Contact from "./contactPage/Contact";
+import Login from "./signinPage/signin";
+import CreateAccount from "./createaacountPage/createAccount";
+import CartPage from "./cartPage/CartPage";
+import IndividualProduct from "./individualProductPage/IndividualProduct";
 
-import 'swiper/css';
+import reportWebVitals from "./reportWebVitals";
+
+import "swiper/css";
 
 // bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 // fonts and icons
-import '././assets/css/icofont.min.css';
-import '././assets/css/animate.css';
-import '././assets/css/style.min.css';
+import "././assets/css/icofont.min.css";
+import "././assets/css/animate.css";
+import "././assets/css/style.min.css";
 
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children:[
-      {path:"/", element:<Home/>},
-      {path: "/about", element: <About/>},
-      {path: "/contact",element: <Contact/>},
-      {path: "/login",element: <Login/>},
-      {path: "/sign-up",element: <CreateAccount/>},
-      {path: "/cart-page",element: <CartPage/>}
-      
-      
-    ]
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/login", element: <Login /> },
+      { path: "/sign-up", element: <CreateAccount /> },
+      { path: "/cart-page", element: <CartPage /> },
+      { path: "/product", element: <IndividualProduct /> },
+    ],
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
