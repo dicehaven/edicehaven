1305158391af68611218c58bb5205e98a6637770
SeanKim <k72591@gmail.com>
2024-03-21 09:39:33 -0400

    get single/all product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,8 +2,6 @@ import ProductModel from "../models/product.js"
 import express from 'express';
 const router = express.Router();
 
-  
-  
   //Create single product
   router.post("/", async(req,res)=>{
     const newProduct = new ProductModel(req.body);
@@ -41,4 +39,40 @@ const router = express.Router();
     }
   });
   
+  //get single product
+  router.get("/:id", async (req, res) => {
+    try {
+      const product = await ProductModel.findById(req.params.id);
+      res.status(200).json(product);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  //get all products
+  router.get("/", async (req, res) => {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
+    try {
+      let products;
+  
+      if (qNew) {
+        products = await ProductModel.find().sort({ createdAt: -1 }).limit(1);
+      } else if (qCategory) {
+        products = await ProductModel.find({
+          categories: {
+            $in: [qCategory],
+          },
+        });
+      } else {
+        products = await ProductModel.find();
+      }
+  
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
   export default router;
