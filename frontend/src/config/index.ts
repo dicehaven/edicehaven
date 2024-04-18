1ad904ca0631c2cced688d2f6ef80d0340bf2f44
ajromeroch <ale.romero07@gmail.com>
2024-04-18 19:52:56 -0400

    testing deployment

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,2 +1,3 @@
 const { NODE_ENV } = process.env;
+console.log("this is the node env", process.env);
 export default NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://edicehaven.onrender.com';
